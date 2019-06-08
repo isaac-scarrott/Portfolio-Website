e83642677aa2c205a9ee13523fe44d5f3903194b
Isaac Scarrott <isaac.scarrott11@gmail.com>
2019-06-08 22:57:24 +0100

    V1.11
    Added comments to HTML and script files

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Used to rotate the navigation toggler on mobile devices so it flips horizontally
 $( "#navtog" ).click(function() {
     if (  $( this ).css( "transform" ) == 'none' ){
         $(this).css("transform","rotateX(180deg)");

85bc837a6d0ec6d539fab20a81993da43ed0bcf3
Isaac <isaac.scarrott11@gmail.com>
2019-07-27 17:31:02 +0100

    Home Project and Blog templates are in. Very basic

--- React-TS/src/ts/shared/index.tsx
+++ React-TS/src/ts/shared/index.tsx
@@ -1,4 +1,4 @@
-// TODO: shrink by function ot save code maybe?
+// TODO: shrink by function to save code maybe?
 
 import React, { createContext, Fragment, useEffect, useState, useContext } from 'react'
 import ReactDOM from 'react-dom'
@@ -16,10 +16,25 @@ function Index() {
             <pages.Provider value={[currentPage, setcurrentPage]}>
                 <NavBar />
             </pages.Provider>
+
+            {(currentPage === 'projects') && <Projects />}
+            
+            {(currentPage === 'blog') && <Blogs />}
+
+            {(currentPage === 'aboutme') && <Projects />}
+
             <SocialBar />
         </Fragment>
     )
 }
+
+function AwesomeFontButton(props) {
+    return (
+        <a href={props.link} target="_blank" rel="noopener noreferrer" className={`afb ${props.iconclassName}`}></a>
+    )
+}
+
+/*---------------------------------------------------TITLE---------------------------------------------------*/
 
 function IsaacScarrottTitle(props) {
     useEffect(() => {
@@ -33,6 +48,8 @@ function IsaacScarrottTitle(props) {
         <div id='isaacScarrottTitle'>Isaac Scarrott</div>
     )
 }
+
+/*---------------------------------------------------NAVBAR---------------------------------------------------*/
 
 function NavBar() {
     const [currentPage, setcurrentPage] = useContext(pages);
@@ -48,28 +65,115 @@ function NavBar() {
     )
 }
 
+/*---------------------------------------------------SOCIAL_BAR---------------------------------------------------*/
+
 function SocialBar() {
     return (
         <nav id='socialBar'>
-            <SocialBarButton
-                iconClassName='fab fa-linkedin'
+            <AwesomeFontButton
+                iconclassName='fab fa-linkedin'
                 link='https://www.linkedin.com/in/isaac-scarrott/'
             />
-            <SocialBarButton
-                iconClassName='fab fa-github'
+            <AwesomeFontButton
+                iconclassName='fab fa-github'
                 link='https://github.com/isaac-scarrott'
             />
-            <SocialBarButton
-                iconClassName='fas fa-address-book'
+            <AwesomeFontButton
+                iconclassName='fas fa-address-book'
                 link='http://isaacscarrott.co.uk/Isaac%20Scarrott%20CV.pdf'
             />
         </nav>
     )
 }
 
-function SocialBarButton(props) {
+/*---------------------------------------------------PROJECTS---------------------------------------------------*/
+
+function Projects(props) {
     return (
-        <a href={props.link} target="_blank" rel="noopener noreferrer" className={props.iconClassName}></a>
+        <Fragment>
+            <Project />
+            <hr className='projectBreaker'/>
+            <Project />
+            <hr className='projectBreaker'/>
+            <Project />
+            <hr className='projectBreaker'/>
+            <Project />
+            <hr className='projectBreaker'/>
+            <Project />
+        </Fragment>
+    )
+}
+
+function Project(props) {
+    return (
+        <div className='project'>
+            <div className='textAndButtons'>
+                <div className='text'>
+                    <div className='title'>Deep Learning To Analyse The Sentiment Of Tweets</div>
+                    <div className='body'>
+                        Final Year University Project where I compared a multi layer perceptron,
+                        convolutional neural network and a recurrent neural network's performance at performing sentiment analysis on
+                        tweets. This model was then implemented into a basic web application where the user can analsye the sentiment
+                        of trends on twitter
+                    </div>
+                </div>
+                <div className='buttons'>
+                    <AwesomeFontButton
+                        iconclassName='fas fa-desktop'
+                        link='http://isaacscarrott.co.uk/Isaac%20Scarrott%20CV.pdf'
+                    />
+                    <AwesomeFontButton
+                        iconclassName='fab fa-github-square'
+                        link='http://isaacscarrott.co.uk/Isaac%20Scarrott%20CV.pdf'
+                    />
+                </div>
+            </div>
+            <div className='skills'>
+                <div>skill 1</div>
+                <div>skill 2</div>
+                <div>skill 3</div>
+                <div>skill 4</div>
+                <div>skill 5</div>
+            </div>
+        </div>
+
+    )
+}
+
+/*---------------------------------------------------PROJECTS---------------------------------------------------*/
+
+function Blogs(props) {
+    return (
+        <Fragment>
+            <Blog />
+            <Blog />
+            <Blog />
+            <Blog />
+            <Blog />
+        </Fragment>
+    )
+}
+
+function Blog(props) {
+    return (
+        <div className='blog'>
+            <div className='title'>First post & building the website</div>
+            <div className='date'>02/06/2019</div>
+            <hr className='blogBreaker'></hr>
+            <div className='body'>  This is my first blog post and I don't really know what I'm doing but maybe I'll get better at these as I go on. These will describe my thought process and hopefully will help me to reflect on projects better. 
+                                    <br/><br/>
+                                    Anyway onto the website. I made this website to showcase my projects (although there are not many at the moment), to create this blog and maybe some other things. The idea behind it was to create as simple as possible website that will allow people who visit it to be able to navigate it as quickly as possible and there to be no extra infomation than is needed. The only issue that I see with this website is the fact the link the my CV is a addres book looking icon. This was used as awesome fonts don't have an icon suitable for a CV however I will be looking to change this very soon. I also might get my parents (average tech savvy) and my brother (more tech savvy than my parents) to have a look at it and test the user interface to give me some feedback. I am also looking to complete the about me page tomorrow so the basic website will be completed and can post it to Linkedin etc. 
+                                    <br/><br/>
+                                    Anyway, my writing skills clearly need to improve and hopefully that is something that will be seen looking back on this post in time to come.</div>
+        </div>
+    )
+}
+
+/*---------------------------------------------------ABOUT-ME---------------------------------------------------*/
+
+function AboutMe(props) {
+    return (
+        <div></div>
     )
 }
 

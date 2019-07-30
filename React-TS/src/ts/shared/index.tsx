8cdf0919b421f5e7845e3d8f7d8f3513424a1673
Isaac <isaac.scarrott11@gmail.com>
2019-07-30 22:11:06 +0100

    about page started

--- React-TS/src/ts/shared/index.tsx
+++ React-TS/src/ts/shared/index.tsx
@@ -1,9 +1,11 @@
-// TODO: shrink by function to save code maybe?
+// TODO:    Shrink by function to save code maybe?
+//          Get rid of provier
 
 import React, { createContext, Fragment, useEffect, useState, useContext } from 'react'
 import ReactDOM from 'react-dom'
 
 const pages = createContext('home')
+const profileTab = createContext('profile')
 
 function Index() {
     const [currentPage, setcurrentPage] = useState('home')
@@ -13,15 +15,15 @@ function Index() {
             <IsaacScarrottTitle
                 currentPage={currentPage}
             />
-            <pages.Provider value={[currentPage, setcurrentPage]}>
+            <pages.Provider value={setcurrentPage}>
                 <NavBar />
             </pages.Provider>
 
             {(currentPage === 'projects') && <Projects />}
-            
+
             {(currentPage === 'blog') && <Blogs />}
 
-            {(currentPage === 'aboutme') && <Projects />}
+            {(currentPage === 'aboutme') && <AboutMe />}
 
             <SocialBar />
         </Fragment>
@@ -39,8 +41,8 @@ function AwesomeFontButton(props) {
 function IsaacScarrottTitle(props) {
     useEffect(() => {
         if (props.currentPage !== 'home') {
-            document.getElementById('isaacScarrottTitle').style.cssText = 'font-size: 1vw; padding-top: 1%; letter-spacing: 0.08vw;'
-            document.getElementById('navBar').style.cssText = 'font-size: 1.7vw; padding-top: 1%;'
+            document.getElementById('isaacScarrottTitle').style.cssText = 'font-size: 1.2vw; padding-top: 1%; letter-spacing: 0.08vw;'
+            document.getElementById('navBar').style.cssText = 'font-size: 2vw; padding-top: 1%;'
         }
     })
 
@@ -52,7 +54,7 @@ function IsaacScarrottTitle(props) {
 /*---------------------------------------------------NAVBAR---------------------------------------------------*/
 
 function NavBar() {
-    const [currentPage, setcurrentPage] = useContext(pages);
+    const setcurrentPage = useContext(pages);
 
     return (
         <nav id='navBar'>
@@ -92,13 +94,13 @@ function Projects(props) {
     return (
         <Fragment>
             <Project />
-            <hr className='projectBreaker'/>
+            <hr className='projectBreaker' />
             <Project />
-            <hr className='projectBreaker'/>
+            <hr className='projectBreaker' />
             <Project />
-            <hr className='projectBreaker'/>
+            <hr className='projectBreaker' />
             <Project />
-            <hr className='projectBreaker'/>
+            <hr className='projectBreaker' />
             <Project />
         </Fragment>
     )
@@ -160,11 +162,11 @@ function Blog(props) {
             <div className='title'>First post & building the website</div>
             <div className='date'>02/06/2019</div>
             <hr className='blogBreaker'></hr>
-            <div className='body'>  This is my first blog post and I don't really know what I'm doing but maybe I'll get better at these as I go on. These will describe my thought process and hopefully will help me to reflect on projects better. 
-                                    <br/><br/>
-                                    Anyway onto the website. I made this website to showcase my projects (although there are not many at the moment), to create this blog and maybe some other things. The idea behind it was to create as simple as possible website that will allow people who visit it to be able to navigate it as quickly as possible and there to be no extra infomation than is needed. The only issue that I see with this website is the fact the link the my CV is a addres book looking icon. This was used as awesome fonts don't have an icon suitable for a CV however I will be looking to change this very soon. I also might get my parents (average tech savvy) and my brother (more tech savvy than my parents) to have a look at it and test the user interface to give me some feedback. I am also looking to complete the about me page tomorrow so the basic website will be completed and can post it to Linkedin etc. 
-                                    <br/><br/>
-                                    Anyway, my writing skills clearly need to improve and hopefully that is something that will be seen looking back on this post in time to come.</div>
+            <div className='body'>  This is my first blog post and I don't really know what I'm doing but maybe I'll get better at these as I go on. These will describe my thought process and hopefully will help me to reflect on projects better.
+                                    <br /><br />
+                Anyway onto the website. I made this website to showcase my projects (although there are not many at the moment), to create this blog and maybe some other things. The idea behind it was to create as simple as possible website that will allow people who visit it to be able to navigate it as quickly as possible and there to be no extra infomation than is needed. The only issue that I see with this website is the fact the link the my CV is a addres book looking icon. This was used as awesome fonts don't have an icon suitable for a CV however I will be looking to change this very soon. I also might get my parents (average tech savvy) and my brother (more tech savvy than my parents) to have a look at it and test the user interface to give me some feedback. I am also looking to complete the about me page tomorrow so the basic website will be completed and can post it to Linkedin etc.
+                                    <br /><br />
+                Anyway, my writing skills clearly need to improve and hopefully that is something that will be seen looking back on this post in time to come.</div>
         </div>
     )
 }
@@ -172,8 +174,46 @@ function Blog(props) {
 /*---------------------------------------------------ABOUT-ME---------------------------------------------------*/
 
 function AboutMe(props) {
+
+    const [currentTab, setCurrentTab] = useState('profile');
+
     return (
-        <div></div>
+        <div id='aboutMe'>
+            <div id='tabBar'>
+                <a onClick={() => setCurrentTab('profile')}>Profile</a>
+                <a onClick={() => setCurrentTab('experience')}>Experience & Education</a>
+                <a onClick={() => setCurrentTab('skills')}>Skills</a>
+                <a onClick={() => setCurrentTab('certificates')}>Awards & Certificates</a>
+            </div>
+
+            {(currentTab === 'profile') && <Profile />}
+
+            {/* {(currentTab === 'experience') && <Expe />}
+
+            {(currentTab === 'education') && <Projects />}
+
+            {(currentTab === 'skills') && <Projects />}
+
+            {(currentTab === 'certificates') && <Projects />} */}
+
+
+
+
+
+
+        </div>
+    )
+}
+
+function Profile(props) {
+    return (
+        <div class='profile'>
+            <img src="img/profileBW.jpg" alt="Isaac Scarrott" id="image" title="" ></img>
+            <div id='body'>
+                <div id='nameAge'>Isaac Scarrott, 20</div>
+                <div id='bio'>University of Lincoln Computer Science graduate currently working as a junior software engineer for Bytron Aviation Systems. Keen software engineer and aspiring data scientist.</div>
+            </div>
+        </div>
     )
 }
 
